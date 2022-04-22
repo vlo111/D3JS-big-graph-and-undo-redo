8805e20c00c6fbf8550b87dbfe36169f2e093844
vlo111 <vvcsharp@gmail.com>
2022-04-22 10:54:07 +0400

    update node svg render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
+import * as d3 from 'd3'
+import './App.css'
+const width = window.innerWidth;
+const height = window.innerHeight - 10;
 
-function App() {
+const generateDataset = () => (
+    Array(10000).fill(100).map((i) => ([
+        Math.floor(Math.random() * width),
+        Math.floor(Math.random() * height),
+        `#${Math.floor(Math.random()*16777215).toString(16)}`
+    ]))
+)
+
+export default function App() {
+    const [dataset, setDataset] = useState(
+        generateDataset()
+    )
+    const ref = useRef()
+
+    useEffect(() => {
+
+        console.log("mounted");
+
+        return () => {
+            const svgElement = d3.select(ref.current)
+
+            const wrapper = svgElement.select('.wrapper');
+
+            const nodes = svgElement.select('.nodes')
+
+            nodes.selectAll(".node")
+                .data(dataset)
+                .join("g")
+                .attr('class', 'node')
+                .append('circle')
+                .attr("cx", d => d[0])
+                .attr("cy", d => d[1])
+                .attr("r",  10)
+                .attr("stroke",  d => d[2]);
+
+            const zoom = d3.zoom()
+                .on('zoom', (ev) => handleZoom(ev, wrapper))
+                .scaleExtent([0.04, 2.5]); // 4% min zoom level to max 250%
+
+            svgElement.call(zoom)
+        };
+
+
+    }, [])
+
+    const handleZoom = (ev, wrapper) => {
+        console.log(ev)
+        const { transform } = ev;
+        wrapper.attr('transform', transform)
+            .attr('data-scale', transform.k)
+            .attr('data-x', transform.x)
+            .attr('data-y', transform.y);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <>
+        <svg
+            width={width}
+            height={height}
+            ref={ref}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <g className="wrapper" transform-origin="top left">
+                <g className='nodes' />
+            </g>
+        </svg>
+      </>
   );
 }
-
-export default App;
